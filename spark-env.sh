d0db48aae5e9a406270eb86a7f675ec57c108538
tm8enk <frederic.comte@insee.fr>
2021-03-06 17:07:41 +0000

    spark dist classpath

--- spark-env.sh
+++ spark-env.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-echo "affichage du path"
-echo $PATH
-echo "affichage de la home java"
-echo $JAVA_HOME
 SPARK_DIST_CLASSPATH=$(hadoop classpath)
-SPARK_DIST_CLASSPATH=$SPARK_DIST_CLASSPATH:$HIVE_HOME/lib
+SPARK_DIST_CLASSPATH=$SPARK_DIST_CLASSPATH:$HIVE_HOME/lib/*
